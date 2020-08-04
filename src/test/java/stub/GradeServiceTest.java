3cf90f708cc76910c24acf0ef8c9a8fa0a636c05
scattter <413365742@qq.com>
2020-08-04 22:15:11 +0800

    SecurityCenterTest

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,7 +1,17 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 public class GradeServiceTest {
     /* 需求描述：
@@ -10,6 +20,7 @@ public class GradeServiceTest {
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+
+        // Assertions.assertEquals(90.0, result);
     }
 }
